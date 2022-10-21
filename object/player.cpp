14e0cfaf61d6a3fbd009a586c0a7ca70531fcac0
わしじゃよわし <fko2247045@stu.o-hara.ac.jp>
2022-10-21 12:01:12 +0900

    ショット発生間隔の追加

--- object/player.cpp
+++ object/player.cpp
@@ -9,11 +9,15 @@ namespace
 	// X方向、Y方向の最大速度
 	constexpr float kSpeedMax = 8.0f;
 	constexpr float kAcc = 0.4f;
+
+	//ショットの生成間隔（フレーム数）
+	constexpr int kShotInterval = 8;
 }
 
 Player::Player():
 	m_pMain(nullptr),
 	m_handle(-1),
+	m_shotInterval(0),
 	m_pos(),
 	m_vec()
 {
@@ -30,10 +34,14 @@ void Player::init()
 	m_pos.y = 100.0f;
 	m_vec.x = 0.0f;
 	m_vec.y = 0.0f;
+	m_shotInterval = kShotInterval;
 }
 
 void Player::update()
 {
+	m_shotInterval--;
+	if (m_shotInterval < 0) m_shotInterval = 0;
+
 	// パッド(もしくはキーボード)からの入力を取得する
 	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	if (padState & PAD_INPUT_UP)
@@ -67,8 +75,9 @@ void Player::update()
 	// キー入力処理
 	if (padState & PAD_INPUT_1)
 	{
-		if (m_pMain) {
+		if ((m_pMain) && (m_shotInterval <= 0)) {
 			m_pMain->createShot(m_pos);
+			m_shotInterval = kShotInterval;
 		}
 	}
 
--- object/player.h
+++ object/player.h
@@ -32,6 +32,9 @@ private:
 
 	int m_handle;
 
+	//ショットの発生間隔
+	int m_shotInterval;
+
 	// 表示位置
 	Vec2 m_pos;
 	// 移動
